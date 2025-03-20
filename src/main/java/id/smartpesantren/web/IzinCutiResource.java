ed66ebdb8abbb9c4fa7e1045044a803637dc28bb
Ustadho <cak.ust@gmail.com>
2025-03-20 12:42:30 +0700

    [fix] problem swagger not showing

--- src/main/java/id/smartpesantren/web/IzinCutiResource.java
+++ src/main/java/id/smartpesantren/web/IzinCutiResource.java
@@ -8,11 +8,13 @@ import id.smartpesantren.web.rest.vm.IzinCutiVM;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@RestController("/api/izin-cuti")
+@RestController
+@RequestMapping("/api/izin-cuti")
 public class IzinCutiResource {
     @Autowired
     IzinCutiRepository izinCutiRepository;
